1a0f8308e8ca42283b2b372defe76667ad7ab270
Rickthus <91890703+Rickthus@users.noreply.github.com>
2021-12-12 19:17:13 -0500

    Update PeripheralController.java

--- Retos_Ciclo4/src/main/java/Retos_Ciclo4/Retos_Ciclo4/controller/PeripheralController.java
+++ Retos_Ciclo4/src/main/java/Retos_Ciclo4/Retos_Ciclo4/controller/PeripheralController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/peripheral")
+@RequestMapping("/api/peripherals")
 @CrossOrigin("*")
 public class PeripheralController {
     @Autowired
